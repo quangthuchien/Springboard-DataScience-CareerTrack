2c74bb37ee5b087f4a935f9e1756cc8d95c86aa0
quangthuchien <quangthuchien@yahoo.com>
2017-05-28 18:34:00 +0700

    data for board games

--- Capstone project 1 - Boardgames/Gameboardgeek scrapper by Thaweatherman/extract_ids.py
+++ Capstone project 1 - Boardgames/Gameboardgeek scrapper by Thaweatherman/extract_ids.py
@@ -3,7 +3,7 @@ import csv
 
 filename = 'items.csv'
 reader = csv.reader(open(filename))
-col = reader.next().index('id')
+col = next(reader).index('id')
 ids = [line[col] for line in reader]
 ids = list(set(ids))
 with open('ids.txt', 'w') as f:
